3cacc182f1db7fd5a40a29280092a36d3d999de3
numeru <numeru@naver.com>
2021-12-18 18:44:02 +0900

    docker project

--- nodejs-docker-app/server.js
+++ nodejs-docker-app/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const PORT = 8080;
 
-//APP 
 const app = express();
-app.get('/', (req,res) => {
-    res.send("반가워요!!!!!!!")
+app.get('/', (req, res) => {
+  res.send('hello!!!!');
 });
 
 app.listen(PORT);
-console.log("Server is running")
+console.log('Server is running');
